dadaab76db330ad41000e9ff0e4f004b9b377969
Nahom Tadele <nahom.tadele@student.allenisd.org>
2022-12-07 23:49:30 -0600

    Added comments for clarity

---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,25 @@
+import Swift
+import Foundation
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+//worked on with william and robel
+// Add your code below:
+
+func fibonacci(int: Int) -> Int {
+    print("Calculating fibonacci(\(int))...")
+    if(int <= 2){
+        print("Terminal case, returning 1.")
+        return 1 // Rreturns 1, and no calculation is needed. Both 2 and 1 have the same squence.
+    }
+    else{
+        let outcome =  fibonacci(int: int-1) + fibonacci(int: int-2) // Uses Recursion to calculate the fibonacci sequence.
+        print("Non-terminal case, returning \(outcome).")
+        return outcome //returns the sequence
+    }
+}
+print("The final result is: \(fibonacci(int: n)).") // Prints the sequence.
