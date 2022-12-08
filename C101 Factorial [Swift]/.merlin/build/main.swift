f35308ea7facd9a381f47ebabfc92ece92027e9a
Nahom Tadele <nahom.tadele@student.allenisd.org>
2022-12-08 12:10:33 -0600

    m2222 redo

---
+++ C101 Factorial [Swift]/.merlin/build/main.swift
@@ -0,0 +1,44 @@
+import Swift
+import Foundation
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 3
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+//worked on with william and robel
+
+// Add your code below:
+
+func factorial(n: Int) -> Int {
+    var outcome = 1
+    if(n > 0) {
+        for i in 1...n {
+            outcome *= i
+        }
+    }
+    return outcome
+}
+func outcomePrint(array: [Int]) {
+    let arrayCount = array.count-1
+    for i in 0...arrayCount {
+        if array[arrayCount-i] != 1 {
+            print("Non-terminal case, returning \(array[arrayCount-i]).")
+        } else {
+            print("Terminal case, returning 1.")
+        }
+    }
+    print("The final result is: \(array[0]).")
+
+}
+var x = n
+var outcomeArray : [Int] = []
+while x != 0 {
+    print("Calculating factorial(\(x))...")
+    let outcome = factorial(n: x)
+    outcomeArray.append(outcome)
+    if outcome == 1 {
+        outcomePrint(array: outcomeArray)
+    }
+    x -= 1
+}
---
+++ C102 Fibonacci [Swift]/.merlin/build/main.swift
@@ -0,0 +1,25 @@
+import Swift
+import Foundation
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 6
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+//worked on with william and robel
+// Add your code below:
+
+func fibonacci(int: Int) -> Int {
+    print("Calculating fibonacci(\(int))...")
+    if(int <= 2){
+        print("Terminal case, returning 1.")
+        return 1
+    }
+    else{
+        let outcome =  fibonacci(int: int-1) + fibonacci(int: int-2)
+        print("Non-terminal case, returning \(outcome).")
+        return outcome
+    }
+}
+print("The final result is: \(fibonacci(int: n)).")
