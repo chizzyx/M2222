983f520d29f4dd7aca2b70ff50e6b88494e3b01c
Nahom Tadele <nahom.tadele@student.allenisd.org>
2022-12-07 23:36:32 -0600

    Added comments for clarity

--- C101 Factorial [Swift]/main.swift
+++ C101 Factorial [Swift]/main.swift
@@ -10,7 +10,7 @@ let n = 7
 
 // Add your code below:
 
-func factorial(n: Int) -> Int {
+func factorial(n: Int) -> Int { //This function calculates the factorialm abd will be used multiple times
     var outcome = 1
     if(n > 0) {
         for i in 1...n {
@@ -19,24 +19,24 @@ func factorial(n: Int) -> Int {
     }
     return outcome
 }
-func outcomePrint(array: [Int]) {
+func outcomePrint(array: [Int]) { // This function handles the print statements for everything except the calculating
     let arrayCount = array.count-1
     for i in 0...arrayCount {
         if array[arrayCount-i] != 1 {
             print("Non-terminal case, returning \(array[arrayCount-i]).")
         } else {
-            print("Terminal case, returning 1.")
+            print("Terminal case, returning 1.") // If the number is 1, then the factorial is simply 1 and no calculation is needed
         }
     }
     print("The final result is: \(array[0]).")
 
 }
-func exec() {
+func exec() { // Main function that will be called on 
 var x = n
 var outcomeArray : [Int] = []
 while x != 0 {
     print("Calculating factorial(\(x))...")
-    let outcome = factorial(n: x)
+    let outcome = factorial(n: x) // Calls on the factorial() function.
     outcomeArray.append(outcome)
     if outcome == 1 {
         outcomePrint(array: outcomeArray)
